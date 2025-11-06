db43ea33fa6477118a70db7b8e28bc51fa430d9e
Jeremy Marin <marin.jeremy@gmail.com>
2025-11-06 14:06:11 +0900

    Add language label in settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,8 +4,9 @@ import { useTranslations } from "next-intl";
 export default function SettingsPage() {
   const t = useTranslations('pages.settings');
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold">{t('title')}</h1>
+      <h2 className="text-2xl font-bold">{t('language')}</h2>
       <div className="flex gap-2">
         <LocaleLinks />
       </div>
