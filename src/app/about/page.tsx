017dcf159327155316f3718a57e1172cd71ab760
Jeremy Marin <marin.jeremy@gmail.com>
2025-11-06 10:50:07 +0900

    Add About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import BookwormLogo from "@/app/ui/layout/bookworm-logo";
-import SigninButton from "@/app/ui/auth/signin-button";
 import { getTranslations } from 'next-intl/server';
 import Footer from "@/app/ui/layout/footer";
+import Link from "next/link";
 
-export default async function FrontPage() {
-  const t = await getTranslations('pages.front');
+export default async function AboutPage() {
+  const t = await getTranslations('pages.about');
   return (
     <main className="flex min-h-screen flex-col p-6 gap-6">
       <div className="flex h-18 shrink-0 items-end rounded-lg bg-yellow-200 p-4 md:h-18">
-        <BookwormLogo />
+        <Link href="/">
+          <BookwormLogo />
+        </Link>
       </div>
       <div className="flex grow flex-col gap-4 md:flex-row">
         <div className="mx-auto my-auto py-6">
@@ -21,10 +23,15 @@ export default async function FrontPage() {
           />
         </div>
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal antialiased`}>
-            {t('hero')}
+          <p className={`text-2xl font-bold text-gray-800 antialiased`}>
+            {t('welcome')}
           </p>
-          <SigninButton />
+          <p className={`text-lg text-gray-800 antialiased`}>
+            {t('content')}
+          </p>
+          <Link href="/" className="text-yellow-500 hover:text-yellow-600">
+            {t('back')}
+          </Link>
         </div>
       </div>
       <Footer />
--- src/app/ui/layout/footer.tsx
+++ src/app/ui/layout/footer.tsx
@@ -1,11 +1,12 @@
 import LocaleLinks from "@/app/ui/layout/locale-links";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-50 py-4 rounded-lg">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-2">
         <p className="text-sm text-gray-500">
-          &copy; {new Date().getFullYear()} grosgg
+          &copy; {new Date().getFullYear()} grosgg . <Link href="/about" className="text-yellow-500 hover:text-yellow-600">About</Link>
         </p>
         <div className="flex gap-2">
           <LocaleLinks />
